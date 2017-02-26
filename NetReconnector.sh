105cb31ff4a42512f3c524cb9e04e47ed3064fbb
ntoff <norahtihtzoff@gmail.com>
2017-02-27 09:55:32 +1000

    for armbian

--- NetReconnector.sh
+++ NetReconnector.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 # by nickwang wxlcat@gmail.com
 
-NetCard=wlan0	 		# the netcard connected to the internet
-PingTarget=www.baidu.com	# the ping target, router ip or website, etc: 192.168.1.1, www.baidu.com
-LogFile=~/NetReconnector.log	# log file
-LogAll=false			# log msg no matter whether the net is connected, used to be check whether the script is running.
+NetCard=wlan0                   # the netcard connected to the internet
+PingTarget=10.0.0.1  # the ping target, router ip or website, etc: 192.168.1.1, www.baidu.com
+LogFile=~/wifisbroken.log       # log file
+LogAll=false                    # log msg no matter whether the net is connected, used to be check whether the script is running.
 
 test -e $LogFile || touch $LogFile
 
 time=$(date "+%Y/%m/%d %H:%M:%S")
 
-ret=$(ping -c 2 -W 1 -I $NetCard $PingTarget | grep 'received' | cut -d ',' -f 2 | cut -d ' ' -f 2)
+ret=$(sudo ping -c 2 -W 1 -I $NetCard $PingTarget | grep 'received' | cut -d ',' -f 2 | cut -d ' ' -f 2)
 
-if [ "$ret" == "0" ]; then
-    sudo ifdown wlan0 && sudo ifup wlan0
+if [ "$ret" != "2" ]; then
+    sudo ifdown $NetCard && sudo modprobe -r r8188eu && sudo modprobe r8188eu && sudo ifup $NetCard
+
     echo "Try Reconnect: $?, $time" | tee -a $LogFile
 else
     if [ "$LogAll" == "true" ]; then
@@ -22,4 +23,5 @@ else
 fi
 
 exit 0
+
 
